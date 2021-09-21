01e25048d91b764f9bec53466ff1a65f9dc2598a
Jean Louis Borba Rosa Salbego <info@jlouis.me>
2021-09-21 23:02:26 +0100

    Sketch 02 - trigonometry

--- sketches/sketch-02.js
+++ sketches/sketch-02.js
@@ -4,6 +4,8 @@ const settings = {
   dimensions: [ 1080, 1080 ]
 };
 
+const degToRad = degrees => degrees / 180 * Math.PI;
+
 const sketch = () => {
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
@@ -11,19 +13,31 @@ const sketch = () => {
 
     context.fillStyle = 'black';
 
-    const x = width * .5;
-    const y = height * .5;
-    const w = width * .3;
-    const h = height * .3;
+    const cx = width * .5;
+    const cy = height * .5;
+    const w = width * .01;
+    const h = height * .1;
+    let x, y;
 
-    context.save();
-    context.translate(x, y);
-    context.rotate(.3);
+    const num = 12;
+    const radius = width * .3;
 
-    context.beginPath();
-    context.rect(-w * .5, -h * .5, w, h);
-    context.fill();
-    context.restore();
+    for (let i = 0; i < num; i++) {
+      const slice = degToRad(360 / num);
+      const angle = slice * i;
+
+      x = cx + radius * Math.sin(angle);
+      y = cy + radius * Math.cos(angle);
+
+      context.save();
+      context.translate(x, y);
+      context.rotate(-angle);
+
+      context.beginPath();
+      context.rect(-w * .5, -h * .5, w, h);
+      context.fill();
+      context.restore();
+    }
   };
 };
 
