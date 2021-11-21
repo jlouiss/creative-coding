d7452620d7cddd5d9560d9c1f7785aa2534a74fe
Jean Louis Borba Rosa Salbego <info@jlouis.me>
2021-11-21 23:51:07 +0000

    Sketch 04 - add noise

--- sketches/sketch-04.js
+++ sketches/sketch-04.js
@@ -1,20 +1,21 @@
-const canvasSketch = require('canvas-sketch');
+import { canvasSketch } from 'canvas-sketch';
+import { math, random } from 'canvas-sketch-util';
 
 const settings = {
   animate: true,
-  dimensions: [ 1080, 1080 ]
+  dimensions: [1080, 1080],
 };
 
 const COLUMNS = 10;
 const ROWS = 10;
 
 const sketch = () => {
-  return ({ context: ctx, width: w, height: h }) => {
-    ctx.fillStyle = 'white';
+  return ({ context: ctx, width: w, height: h, frame }) => {
+    ctx.fillStyle = '#111';
     ctx.fillRect(0, 0, w, h);
 
-    const gridWidth = w * .8;
-    const gridHeight = h * .8;
+    const gridWidth = w * 0.8;
+    const gridHeight = h * 0.8;
     const marginX = (w - gridWidth) / 2;
     const marginY = (h - gridHeight) / 2;
     const cellWidth = gridWidth / COLUMNS;
@@ -28,10 +29,16 @@ const sketch = () => {
 
       const cx = column * cellWidth;
       const cy = row * cellHeight;
-      const cw = cellWidth * .8;
-      const ch = cellHeight * .8;
-      const x = cx + cw / 2 + marginX;
-      const y = cy + ch / 2 + marginY;
+      const cw = cellWidth * 0.8;
+      const ch = cellHeight * 0.8;
+
+      // add noise (0 - 1)
+      const n = random.noise2D(cx + (frame % Number.MAX_SAFE_INTEGER) * 10, cy, 0.001);
+      const angle = n * Math.PI * 0.5;
+      const scale = math.mapRange(n, -1, 1, .1, 12);
+
+      const translateX = cx + cw / 2 + marginX;
+      const translateY = cy + ch / 2 + marginY;
 
       ctx.save();
 
@@ -46,7 +53,8 @@ const sketch = () => {
       // // Move the cell to have (0, 0) at the center;
       // ctx.translate(cw / 2, ch / 2);
 
-      ctx.translate(x, y);
+      ctx.translate(translateX, translateY);
+      ctx.rotate(angle);
 
       ctx.beginPath();
 
@@ -60,7 +68,8 @@ const sketch = () => {
       ctx.moveTo(-cw / 2, 0);
       ctx.lineTo(cw / 2, 0);
 
-      ctx.lineWidth = 4;
+      ctx.strokeStyle = '#ddd';
+      ctx.lineWidth = scale;
       ctx.stroke();
 
       ctx.restore();
