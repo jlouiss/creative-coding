e1b7e0add6955b6a8cf81375a1711e28e90ae3b5
jean <info@jlouis.me>
2022-02-09 21:10:52 +0000

    Pixelise something

--- sketches/sketch-05.js
+++ sketches/sketch-05.js
@@ -23,19 +23,30 @@ function loadFont() {
 }
 
 let text = 'A';
-let fontSize = 1200;
 let fontFamily = 'Playfair Display';
+const cellSize = 20;
 
-const sketch = () => {
+const typeCanvas = document.createElement('canvas');
+const typeCtx = typeCanvas.getContext('2d');
+
+const sketch = ({ context: ctx, width, height }) => {
+  const cols = Math.floor(width / cellSize);
+  const rows = Math.floor(height / cellSize);
+  const cells = cols * rows;
+  const fontSize = cols;
+
+  typeCanvas.width = cols;
+  typeCanvas.height = rows;
+
   return ({ context: ctx, width, height }) => {
-    ctx.fillStyle = 'white';
-    ctx.fillRect(0, 0, width, height);
+    typeCtx.fillStyle = 'black';
+    typeCtx.fillRect(0, 0, cols, rows);
 
-    ctx.fillStyle = 'black';
-    ctx.font = `${fontSize}px '${fontFamily}'`;
-    ctx.textBaseline = 'top';
+    typeCtx.fillStyle = 'white';
+    typeCtx.font = `${fontSize}px '${fontFamily}'`;
+    typeCtx.textBaseline = 'top';
 
-    const metrics = ctx.measureText(text);
+    const metrics = typeCtx.measureText(text);
     const {
       actualBoundingBoxAscent: top,
       actualBoundingBoxDescent: bottom,
@@ -47,18 +58,47 @@ const sketch = () => {
     const mw = left + right;
     const mh = top + bottom;
 
-    const x = (width - mw) * 0.5 - mx;
-    const y = (height - mh) * 0.5 - my;
+    const tx = (cols - mw) * 0.5 - mx;
+    const ty = (rows - mh) * 0.5 - my;
 
-    ctx.save();
-    ctx.translate(x, y);
-    ctx.fillText(text, 0, 0);
-    ctx.restore();
+    typeCtx.save();
+    typeCtx.translate(tx, ty);
+    typeCtx.fillText(text, 0, 0);
+    typeCtx.restore();
+
+    const typeData = typeCtx.getImageData(0, 0, cols, rows).data;
+    console.dir(typeData);
+    ctx.drawImage(typeCanvas, 0, 0);
+
+    for (let i = 0; i < cells; i++) {
+      const col = i % cols;
+      const row = Math.floor(i / cols);
+      const x = col * cellSize;
+      const y = row * cellSize;
+
+      const r = typeData[i * 4];
+      const g = typeData[i * 4 + 1];
+      const b = typeData[i * 4 + 2];
+      // const a = typeData[i * 4 + 3];
+
+      ctx.fillStyle = `rgb(${r},${g},${b})`;
+
+      ctx.save();
+
+      ctx.translate(x, y);
+      ctx.translate(cellSize * .5, cellSize * .5);
+      // ctx.fillRect(0, 0, cellSize, cellSize);
+      ctx.beginPath();
+      ctx.arc(0, 0, cellSize / 2, 0, Math.PI * 2);
+      ctx.fill();
+
+      ctx.restore();
+    }
   };
 };
 
 document.addEventListener('keyup', event => {
-  text = event.key;
+  text = event.key.toUpperCase();
   manager.render();
 });
 
