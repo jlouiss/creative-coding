4f740ad82b2b06dff9fb3e540748ff368f2b98bd
Jean <jean@jlouis.me>
2023-04-16 09:35:58 +0100

    Circles

---
+++ sketches/audio/circles.js
@@ -0,0 +1,229 @@
+import canvasSketch from 'canvas-sketch';
+import { math, random } from 'canvas-sketch-util';
+import eases from 'eases';
+import { CircleGridPoint } from '../classes/circle-grid-point';
+import { AudioPlayer } from '../classes/audio-player';
+
+let manager;
+let audioPlayer;
+
+const seed = random.getRandomSeed();
+const settings = {
+  dimensions: [2048, 2048],
+  animate: true,
+  name: seed,
+};
+
+const parameters = {
+  amplitude: 3,
+  bg: '#0f0f0f',
+  fg: '#ececec',
+  frequency: 0.0008,
+  cols: 64,
+  rows: 64,
+  gridHeightScale: 0.84,
+  gridWidthScale: 0.84,
+  easingFunctionX: eases.cubicOut,
+  easingFunctionY: eases.cubicIn,
+};
+
+const sketch = ({ height, width }) => {
+  const cols = parameters.cols;
+  const rows = parameters.rows;
+  const easingFunctionX = parameters.easingFunctionX;
+  const easingFunctionY = parameters.easingFunctionY;
+  const gw = width * parameters.gridWidthScale;
+  const gh = height * parameters.gridHeightScale;
+  const cw = gw / cols;
+  const ch = gh / rows;
+  const mx = (width - gw) / 2;
+  const my = (height - gh) / 2;
+  const points = [];
+  const ballBoundaries = {
+    xMin: mx + cw * 6,
+    xMax: gw - cw * 6,
+    yMin: my + ch * 6,
+    yMax: gh - ch * 6,
+  };
+  let ball;
+  let bins = [];
+
+  document.querySelector('canvas').style.boxShadow =
+    '0px 0px 200px 0 rgba(255, 255, 255, .2)';
+  document.querySelector('body').style.background = 'black';
+
+  for (let i = 0; i < rows * cols; i++) {
+    const col = i % cols;
+    const row = Math.floor(i / cols);
+
+    if (col < 6 || row < 6) {
+      continue;
+    }
+
+    let x = math.mapRange(
+      easingFunctionX(col),
+      easingFunctionX(2),
+      easingFunctionX(cols - 1),
+      gw - cw / 2,
+      cw,
+      true
+    );
+    let y = math.mapRange(
+      easingFunctionY(row),
+      easingFunctionY(2),
+      easingFunctionY(rows - 1),
+      gh - ch / 2,
+      ch,
+      true
+    );
+    let size = math.mapRange(
+      eases.quintIn(col + row) * i,
+      0,
+      eases.quintIn(cols + rows - 2) * rows * cols,
+      4,
+      20,
+      true
+    );
+
+    points.push(
+      new CircleGridPoint({
+        color: parameters.fg,
+        x,
+        y,
+        size,
+      })
+    );
+  }
+
+  ball = new CircleGridPoint({
+    x: random.rangeFloor(ballBoundaries.xMin, ballBoundaries.xMax),
+    y: random.rangeFloor(ballBoundaries.yMin, ballBoundaries.yMax),
+    size: 20,
+    dampening: 0.5,
+  });
+
+  for (let i = 0; i < points.length; i++) {
+    const bin = random.value() < 0.76 ? random.rangeFloor(4, 64) : 0;
+    bins.push(bin);
+  }
+
+  return ({ context: ctx, width, height, frame: f }) => {
+    ctx.fillStyle = parameters.bg;
+    ctx.fillRect(0, 0, width, height);
+
+    if (!audioPlayer || !audioPlayer.context) {
+      return;
+    }
+    audioPlayer.getFloatFrequencyData();
+
+    ctx.save();
+    ctx.translate(mx, my);
+    ctx.translate(cw / 2, ch / 2);
+    ctx.lineWidth = 4;
+
+    if (ball) {
+      const n = random.noise3D(
+        ball.x,
+        ball.y,
+        (f % Number.MAX_SAFE_INTEGER) * 10,
+        parameters.frequency / 4,
+        parameters.amplitude
+      );
+      ball.ax = Math.cos(n * Math.PI * 2);
+      ball.ay = Math.sin(n * Math.PI * 2);
+      ball.vx += ball.ax;
+      ball.vy += ball.ay;
+      ball.vx *= ball.dampening;
+      ball.vy *= ball.dampening;
+
+      if (ball.x < ballBoundaries.xMin || ball.x > ballBoundaries.xMax) {
+        ball.vx *= -2.2;
+      }
+
+      if (ball.y < ballBoundaries.yMin || ball.y > ballBoundaries.yMax) {
+        ball.vy *= -2.2;
+      }
+
+      ball.x += ball.vx;
+      ball.y += ball.vy;
+    }
+
+    points.sort();
+    for (let i = 0; i < points.length; i++) {
+      const p = points[i];
+      const n = random.noise3D(
+        p.x,
+        p.y,
+        (f % Number.MAX_SAFE_INTEGER) * 10,
+        parameters.frequency,
+        parameters.amplitude
+      );
+      const size = math.mapRange(
+        audioPlayer.audioData[bins[i]],
+        audioPlayer.minDb,
+        audioPlayer.maxDb,
+        p.initialSize * 0.3,
+        p.initialSize * 0.6,
+        true
+      );
+
+      p.size =
+        size *
+        (1.5 -
+          math.mapRange(
+            n,
+            -parameters.amplitude,
+            parameters.amplitude,
+            0,
+            0.9
+          ));
+      p.x = p.initialX + n;
+      p.y = p.initialY + n;
+
+      if (size > p.initialSize * 0.4 && size < p.initialSize * 0.48) {
+        continue;
+      } else if (size >= p.initialSize * 0.57) {
+        p.color = 'red';
+      } else {
+        p.color = p.initialColor;
+      }
+
+      p.update({ cx: ball.x, cy: ball.y });
+      p.draw(ctx);
+    }
+
+    ctx.restore();
+  };
+};
+
+const onMouseUp = () => {
+  if (!audioPlayer || !audioPlayer.context) {
+    audioPlayer = new AudioPlayer({
+      filename: '../assets/02.mp3',
+      settings: {
+        fftSize: 512,
+        smoothingTimeConstant: 0.9,
+      },
+    });
+  }
+
+  if (audioPlayer.audioElement.paused) {
+    audioPlayer.play();
+    if (manager) manager.play();
+  } else {
+    audioPlayer.pause();
+    if (manager) manager.pause();
+  }
+};
+
+const addListeners = () => {
+  window.addEventListener('mouseup', onMouseUp);
+  window.addEventListener('touchstart', onMouseUp);
+};
+
+const start = async () => {
+  addListeners();
+  manager = await canvasSketch(sketch, settings);
+};
+
+start();
---
+++ sketches/classes/circle-grid-point.js
@@ -0,0 +1,79 @@
+import { math, random } from 'canvas-sketch-util';
+
+export class CircleGridPoint {
+  constructor({
+    ax = 0.1,
+    ay = 0,
+    vx = 0,
+    vy = 0,
+    x,
+    y,
+    color,
+    size = 8,
+    dampening = random.range(0.8, 0.95),
+    minDist = random.range(360, 460),
+    pullForce = random.range(0.002, 0.5),
+    pushForce = random.range(0.001, 0.7),
+  }) {
+    // acceleration
+    this.ax = ax;
+    this.ay = ay;
+    // initial position
+    this.ix = x;
+    this.iy = y;
+    // velocity
+    this.vx = vx;
+    this.vy = vy;
+    this.x = x;
+    this.y = y;
+    this.color = color;
+    this.initialColor = color;
+    this.dampening = dampening;
+    this.minDist = minDist;
+    this.pullForce = pullForce;
+    this.pushForce = pushForce;
+    this.size = size;
+    this.initialSize = size;
+    this.initialX = x;
+    this.initialY = y;
+  }
+
+  update({ cx, cy } = { cx: Infinity, cy: Infinity }) {
+    // pull
+    let dx = this.ix - this.x;
+    let dy = this.iy - this.y;
+    let dd = Math.sqrt(dx * dx + dy * dy);
+    this.ax = dx * this.pullForce;
+    this.ay = dy * this.pullForce;
+
+    this.scale = math.mapRange(dd, 0, 200, 1, 3.5);
+
+    // push
+    dx = this.x - cx;
+    dy = this.y - cy;
+
+    dd = Math.sqrt(dx * dx + dy * dy);
+    const diff = this.minDist - dd;
+
+    if (dd < this.minDist) {
+      this.ax += (dx / dd) * diff * this.pushForce;
+      this.ay += (dy / dd) * diff * this.pushForce;
+    }
+
+    this.vx += this.ax;
+    this.vy += this.ay;
+    this.vx *= this.dampening;
+    this.vy *= this.dampening;
+    this.x += this.vx;
+    this.y += this.vy;
+  }
+
+  draw(ctx) {
+    ctx.save();
+    ctx.fillStyle = this.color;
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+    ctx.fill();
+    ctx.restore();
+  }
+}
